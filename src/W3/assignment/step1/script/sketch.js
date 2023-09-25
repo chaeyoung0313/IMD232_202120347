843ab5051e83122ef90bdbc54af77c939b680c42
chaeyoung0313 <143991676+chaeyoung0313@users.noreply.github.com>
2023-09-25 15:17:31 +0900

    09.25

--- src/W3/assignment/step1/script/sketch.js
+++ src/W3/assignment/step1/script/sketch.js
@@ -1,7 +1,7 @@
 let position;
 let velocity;
 let acceleraton;
-let topSpeed = 5;
+let topSpeed = 4;
 
 function setup() {
   setCanvasContainer('mySketchGoesHere', 3, 2, true);
@@ -15,14 +15,25 @@ function draw() {
   checkEdges();
   show();
   let mouse = createVector(mouseX, mouseY);
+  //   let line1 = createVector(50, 50);
+  //   let line2 = createVector(mouseX - 50, mouseY - 50);
+
+  //   drawArrow(line1, line2, 'red');
+  //   drawArrow(line1, line2.limit(35), 'blue');
   //   let position = createVector();
 
-  //   strokeWeight(3);
-  //   stroke(100);
+  strokeWeight(1);
+  stroke(0, 0, 255);
+  line(position.x, position.y, random(width), random(height));
+  stroke(255, 0, 0);
+  line(position.x, position.y, random(width), random(height));
+
+  strokeWeight(3);
+  stroke(0);
   line(position.x, position.y, mouse.x, mouse.y);
 
-  translate(position.x, position.y);
-  line(mouse.x, mouse.y);
+  //   translate(position.x, position.y);
+  //   line(mouse.x, mouse.y);
   //   mouse.sub(position);
 }
 

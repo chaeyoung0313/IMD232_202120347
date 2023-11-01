a0199c6eb57d9e376682132c574ab6c7638a3bd2
chaeyoung0313 <143991676+chaeyoung0313@users.noreply.github.com>
2023-11-01 21:00:15 +0900

    11.01

---
+++ src/W7/assignment/step1/script/Traffic.js
@@ -0,0 +1,18 @@
+// class Traffic {
+//   constructor() {
+//     this.vehicles = [];
+//   }
+
+//   run() {
+//     this.vehicles.forEach((eachVehicle) => {
+//       eachVehicle.update();
+//       eachVehicle.display();
+//     });
+//   }
+
+//   addVehicle(x, y) {
+//     this.vehicles.push(
+//       new Vehicle(x, y, 8, 5, 0.1, color(random(360), 100, 50))
+//     );
+//   }
+// }
